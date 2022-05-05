aa6c7708eabb40a03d5fb5da4e279b7c1297a085
Xavier Salazar <x.salazar1729@gmail.com>
2022-05-05 15:12:00 -0700

    First implementation of image slicing

---
+++ app/index.js
@@ -0,0 +1,54 @@
+const sharp = require("sharp");
+const imageToSlices = require("image-to-slices");
+
+exports.handler = async (event, context) => {
+  console.log(JSON.stringify(event));
+
+  var errorReturnResponse = {
+    cookies: [],
+    isBase64Encoded: false,
+    statusCode: 400,
+    headers: {},
+    body: "",
+  };
+
+  if (event.body) {
+    const fileLocation = "/tmp/tmp.png";
+
+    // Take input data and resize it to be 7 64-px emojis wide
+    sharp(Buffer.from(event.body))
+      .resize(64 * 7)
+      .toFile(fileLocation);
+
+    const metadata = await sharp(fileLocation).metadata();
+
+    const xSlices = [64, 128, 192, 256, 320, 384, 448];
+    const ySlices = [];
+
+    for (var j = 64; j < metadata.height; j += 64) {
+      ySlices.push(j);
+    }
+
+    var returnList;
+
+    imageToSlices(
+      fileLocation,
+      xSlices,
+      ySlices,
+      { saveToDataUrl: true },
+      function (dataUrlList) {
+        returnList = dataUrlList.map((x) => x.dataURI);
+      }
+    );
+
+    console.log(returnList);
+
+    return {
+      cookies: [],
+      isBase64Encoded: true,
+      statusCode: 200,
+      headers: { "content-type": contentType },
+      body: returnList,
+    };
+  }
+};
