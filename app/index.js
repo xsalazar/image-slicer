eee4d23bc074918f44cfb0716db422bc87071062
Xavier Salazar <x.salazar1729@gmail.com>
2022-05-07 00:52:11 -0700

    Update padding logic

--- app/index.js
+++ app/index.js
@@ -24,10 +24,13 @@ exports.handler = async (event, context) => {
 
     // If resulting image height isn't divisible by 64, pad the bottom so it is
     if (height % 64 !== 0) {
+      console.log(`Height: ${height}`);
+      console.log(`height % 64: ${height % 64} pixles over`);
+      console.log(`64 - (height % 64): ${64 - (height % 64)} to add`);
       const buffer = await sharp(fileLocation)
         .extend({
           bottom: 64 - (height % 64),
-          background: { r: 0, g: 0, b: 0, alpha: 1 },
+          background: { r: 0, g: 0, b: 0, alpha: 0 },
         })
         .toBuffer();
 
