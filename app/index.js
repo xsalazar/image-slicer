cfde489c2629e27c4d0a613174e1db2bf4156ea1
Xavier Salazar <x.salazar1729@gmail.com>
2022-09-11 18:08:29 -0700

    First attempt at dynamically generating landscape vs. portrait and keep under 23 JUMBOMOJI limit

--- app/index.js
+++ app/index.js
@@ -2,55 +2,114 @@ const sharp = require("sharp");
 const imageToSlices = require("image-to-slices");
 const { v4: uuidv4 } = require("uuid");
 
+// Slack uses JUMBOMOJI for less than 23 emoji in one message, so we'll have either a (4x5) or a (5x4) mosaic
+// This requires padding either the bottom or the right hand side of the image, depending on which original dimension is longer to preserve landscape vs. portrait
 exports.handler = async (event, context) => {
   console.log(JSON.stringify(event));
 
   if (event.body) {
     try {
-      const fileLocation = `/tmp/${uuidv4()}.png`;
-
-      // Take input data and resize it to be 7 64-px emojis wide, then grab resulting height
-      const resizedHeight = (
-        await sharp(Buffer.from(event.body, "base64"))
-          .resize(64 * 7)
-          .toFile(fileLocation)
-      ).height;
-
-      // If resulting image height isn't divisible by 64, pad the bottom so it is
-      if (resizedHeight % 64 !== 0) {
-        const buffer = await sharp(fileLocation)
-          .extend({
-            bottom: 64 - (resizedHeight % 64),
-            background: { r: 0, g: 0, b: 0, alpha: 0 },
-          })
-          .toBuffer();
-
-        await sharp(buffer).toFile(fileLocation);
-      }
-
-      const ySlices = [64, 128, 192, 256, 320, 384, 448];
-
-      const maxXSlices = Math.floor(resizedHeight / 64);
-      const xSlices = Array.from({ length: maxXSlices }, (_, i) => 64 + i * 64);
-
       var returnList;
-      imageToSlices(
-        fileLocation,
-        xSlices,
-        ySlices,
-        {
-          saveToDataUrl: true,
-          clipperOptions: {
-            canvas: require("canvas"),
-          },
-        },
-        function (dataUrlList) {
-          returnList = dataUrlList.map((x) => x.dataURI);
-        }
-      );
+      const fileLocation = `/tmp/${uuidv4()}.png`;
+      const originalImage = sharp(Buffer.from(event.body, "base64"));
 
-      while (!returnList) {
-        await new Promise((r) => setTimeout(r, 250));
+      // If the original image is taller than it is wider, make it 5 emoji tall and calculate width
+      if (originalImage.height > originalImage.width) {
+        const resizedWidth = (
+          await originalImage
+            .resize(
+              null, // width
+              64 * 5 // height
+            )
+            .toFile(fileLocation)
+        ).width;
+
+        // If resulting image width isn't divisible by 64, pad the right side so it is
+        if (resizedWidth % 64 !== 0) {
+          const buffer = await sharp(fileLocation)
+            .extend({
+              right: 64 - (resizedWidth % 64),
+              background: { r: 0, g: 0, b: 0, alpha: 0 },
+            })
+            .toBuffer();
+
+          await sharp(buffer).toFile(fileLocation);
+        }
+
+        const xSlices = [64, 128, 192, 256, 320]; // Horizontal slices parallel to the x-axis
+        const maxYSlices = Math.floor(resizedWidth / 64);
+        const ySlices = Array.from(
+          { length: maxYSlices },
+          (_, i) => 64 + i * 64
+        );
+
+        imageToSlices(
+          fileLocation,
+          xSlices,
+          ySlices,
+          {
+            saveToDataUrl: true,
+            clipperOptions: {
+              canvas: require("canvas"),
+            },
+          },
+          function (dataUrlList) {
+            returnList = dataUrlList.map((x) => x.dataURI);
+          }
+        );
+
+        while (!returnList) {
+          await new Promise((r) => setTimeout(r, 50));
+        }
+      }
+      // The original image is wider than it is taller, so make it 5 emoji wide and calculate height
+      else {
+        const resizedHeight = (
+          await originalImage
+            .resize(
+              64 * 5, //width
+              null // height
+            )
+            .toFile(fileLocation)
+        ).height;
+
+        // If resulting image height isn't divisible by 64, pad the bottom side so it is
+        if (resizedHeight % 64 !== 0) {
+          const buffer = await sharp(fileLocation)
+            .extend({
+              bottom: 64 - (resizedHeight % 64),
+              background: { r: 0, g: 0, b: 0, alpha: 0 },
+            })
+            .toBuffer();
+
+          await sharp(buffer).toFile(fileLocation);
+        }
+
+        const ySlices = [64, 128, 192, 256, 320]; // Vertical slices parallel to the y-axis
+        const maxXSlices = Math.floor(resizedHeight / 64);
+        const xSlices = Array.from(
+          { length: maxXSlices },
+          (_, i) => 64 + i * 64
+        );
+
+        imageToSlices(
+          fileLocation,
+          xSlices,
+          ySlices,
+          {
+            saveToDataUrl: true,
+            clipperOptions: {
+              canvas: require("canvas"),
+            },
+          },
+          function (dataUrlList) {
+            returnList = dataUrlList.map((x) => x.dataURI);
+          }
+        );
+
+        while (!returnList) {
+          await new Promise((r) => setTimeout(r, 50));
+        }
       }
 
       return {
