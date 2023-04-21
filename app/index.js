c15844e7100591480de266f7f16bd8db6786cabc
Xavier Salazar <x.salazar1729@gmail.com>
2023-04-20 17:26:21 -0700

    Take the image's default orientation before resizing

--- app/index.js
+++ app/index.js
@@ -22,6 +22,7 @@ exports.handler = async (event, context) => {
       if (originalHeight > originalWidth) {
         const resizedWidth = (
           await originalImage
+            .rotate()
             .resize(
               null, // width
               64 * 5 // height
@@ -70,6 +71,7 @@ exports.handler = async (event, context) => {
       // The original image is square, fall back to (4x4) to preserve aspect ratio while still under JUMBOMOJI limit
       else if (originalHeight === originalWidth) {
         await originalImage
+          .rotate()
           .resize(
             64 * 4, // width
             null // height
@@ -103,6 +105,7 @@ exports.handler = async (event, context) => {
 
         const resizedHeight = (
           await originalImage
+            .rotate()
             .resize(
               64 * 5, // width
               null // height
