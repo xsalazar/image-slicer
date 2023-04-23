4d487639ab26e7357712ce65ea966ad02afc1a0f
Xavier Salazar <x.salazar1729@gmail.com>
2023-04-22 17:46:43 -0700

    Deprecate Lambda Layer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,17 +3,9 @@ resource "aws_lambda_function" "instance" {
   filename      = "${path.module}/dummy-lambda-package/lambda.zip" // Simple hello world application
   role          = aws_iam_role.instance.arn
   handler       = "index.handler"
-  runtime       = "nodejs14.x"
+  runtime       = "nodejs16.x"
   timeout       = 30   // seconds
   memory_size   = 1536 // MB
-
-  // See: https://www.jvt.me/posts/2022/04/08/node-canvas-lambda/
-  layers = ["arn:aws:lambda:us-west-2:368081326042:layer:canvas-nodejs:1"]
-  environment {
-    variables = {
-      LD_PRELOAD : "/var/task/node_modules/canvas/build/Release/libz.so.1"
-    }
-  }
 
   // Since CI/CD will deploy this application externally, these do not need to be tracked after creation
   lifecycle {
